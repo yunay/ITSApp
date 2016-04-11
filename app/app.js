fe09bfeaf8edf980738f9372060af709aae3168b
Hamza <yhamza@vgb.bg>
2016-04-11 14:28:43 +0300

    Added validation of user panel

--- app/app.js
+++ app/app.js
@@ -2,11 +2,33 @@
 
 // Declare app level module which depends on views, and components
 angular.module('ITSApp', [
-    'ngRoute',
-    'ITSApp.projects',
-    'ITSApp.user',
-    'ITSApp.dashboard',
-    'ITSApp.version'
-]).config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.otherwise({redirectTo: '/projects'});
-}]);
+        'ngRoute',
+        'ITSApp.projects',
+        'ITSApp.user',
+        'ITSApp.dashboard',
+        'ITSApp.version'
+    ])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/projects'});
+    }])
+    .factory('myNotifications', [function () {
+        function notify (textNotification,typeOfNotification){
+            noty({
+                layout: 'topCenter',
+                timeout: 3000,
+                theme: 'relax',
+                type: ''+typeOfNotification+'',
+                text: ''+textNotification+'',
+                animation: {
+                    open: {height: 'toggle'},
+                    close: {height: 'toggle'},
+                    easing: 'swing', // easing
+                    speed: 500 // opening & closing animation speed
+                }
+            });
+        }
+
+        return {
+            notify : notify
+        }
+    }]);
--- app/views/user/user-controller.js
+++ app/views/user/user-controller.js
@@ -10,6 +10,62 @@ angular.module('ITSApp.user', ['ngRoute'])
             })
     }])
 
-    .controller('UserController', [function () {
+    .controller('UserController', [
+        '$scope',
+        'myNotifications',
+        function ($scope,myNotifications) {
+            $scope.login = function(user){
+                var isEveryThingTrue = true;
+                if(user == undefined || user.username.length < 1){
+                    isEveryThingTrue = false;
+                    myNotifications.notify('Username is required','error')
+                }else{
+                    isEveryThingTrue = true;
+                }
 
+                if(user == undefined || user.password.length < 1) {
+                    isEveryThingTrue = false;
+                    myNotifications.notify('Password is required','error')
+                }else{
+                    isEveryThingTrue = true;
+                }
+
+                if(isEveryThingTrue){
+                    console.log(user);
+                }
+            };
+
+            $scope.register = function(user){
+                var isEveryThingTrue = true;
+                if(user == undefined){
+                    myNotifications.notify('Please fill correctly the registration form','error');
+                    isEveryThingTrue = false;
+                }else{
+                    if(user.username == undefined || user.username.length < 1){
+                        myNotifications.notify('Username is required','error');
+                        isEveryThingTrue = false;
+                    }
+                    if(user.password == undefined || user.password.length < 1){
+                        myNotifications.notify('Password is required','error');
+                        isEveryThingTrue = false;
+                    }else{
+                        if(user.password != user.confirmPassword){
+                            myNotifications.notify('Please confirm password correctly','error');
+                            isEveryThingTrue = false;
+                        }
+                    }
+                    if(user.name == undefined || user.name.length < 1){
+                        myNotifications.notify('Name is required','error');
+                        isEveryThingTrue = false;
+                    }
+                    if(user.email == undefined || user.email.length < 1){
+                        myNotifications.notify('Email is required','error');
+                        isEveryThingTrue = false;
+                    }
+                }
+
+                if(isEveryThingTrue){
+                    console.log(user);
+                }
+            };
     }]);
